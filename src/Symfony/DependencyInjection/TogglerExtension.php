1eb6aefc45e182de24ffabef9a4e724da388d7f8
Pierre du Plessis <pierre@pcservice.co.za>
2015-09-20 16:01:26 +0200

    Fix toggler config nog always setting when cache is cleared in Symfony

--- src/Symfony/DependencyInjection/TogglerExtension.php
+++ src/Symfony/DependencyInjection/TogglerExtension.php
@@ -13,7 +13,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Toggler\Config;
 
 class TogglerExtension extends Extension
 {
@@ -25,7 +24,7 @@ class TogglerExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        Config::instance()->setConfig(($config['config']));
+        $container->setParameter('toggler.config', $config['config']);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
--- src/Symfony/TogglerBundle.php
+++ src/Symfony/TogglerBundle.php
@@ -10,7 +10,14 @@
 namespace Toggler\Symfony;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Toggler\Config;
 
 class TogglerBundle extends Bundle
 {
+    public function boot()
+    {
+        $config = $this->container->getParameter('toggler.config');
+
+        Config::instance()->setConfig($config);
+    }
 }
