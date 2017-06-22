512080fa7732d205a63d8450b952cb88e5a26471
Pierre du Plessis <pierre@pcservice.co.za>
2017-06-22 23:14:13 +0200

    Update Travis config to test on different environments (#8)
    * Update Travis config to test on different environments
    
    * Add class alias for older versions of PHPUnit
    
    * Remove check for class alias
    
    * Fix CS
    
    * Move class alias to a bootsrap file
    
    * Add header to bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the Toggler package.
+ *
+ * (c) SolidWorx <open-source@solidworx.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+class_alias(PHPUnit\Framework\TestCase::class, 'PHPUnit_Framework_TestCase');
